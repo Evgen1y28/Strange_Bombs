df1d852f004b26e20d7ae2cde495aa51e4960a3a
Katherine <katevi33@gmail.com>
2018-04-11 22:39:03 +0300

    11.04.2018 modified FileSaverService

--- StangeBombs/FileSaverService.cs
+++ StangeBombs/FileSaverService.cs
@@ -5,7 +5,7 @@ using System.IO;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ModuleTwo
+namespace StangeBombs
 {
     public interface IFileSaverService
     {
@@ -16,19 +16,26 @@ namespace ModuleTwo
     {
         public bool SaveFile(string content, string location, string fileName)
         {
-            var result = false;             
-            string fileName2 = fileName + "_reverse" + ".txt";
+            var result = false;            
             string content_reverse = new string(content.ToCharArray().Reverse().ToArray());           
-            if(!File.Exists(location + @"\" + fileName2))
+            string location_reverse = new string(location.ToCharArray().Reverse().ToArray());
+            string [] words=location_reverse.Split(new char[] {'\\'});
+            string reverse_name = words[0];            
+            string new_name2= new string(reverse_name.ToCharArray().Reverse().ToArray());
+            string[]w = new_name2.Split(new char[] { '.'});
+            string new_name= w[0];
+            string new_path = location.Substring(0, location.Length - new_name2.Length);           
+            string fileName2 = new_name + "_reverse" + ".txt";            
+            if (!File.Exists(new_path + @"\" + fileName2))
             {
-                StreamWriter textfileName2 = new StreamWriter(location + @"\" + fileName2);
+                StreamWriter textfileName2 = new StreamWriter(new_path + @"\" + fileName2);
                 textfileName2.Write(content_reverse);
                 textfileName2.Close();
-                string [] s = File.ReadAllLines(location + @"\" + fileName2);
+                string [] s = File.ReadAllLines(new_path + @"\" + fileName2);
                 string s1 = String.Concat<string>(s);
-                if (File.Exists(location + @"\" + fileName2) && s1 == content_reverse)
+                if (File.Exists(new_path + @"\" + fileName2) && s1 == content_reverse)
                 { result = true;
-                    Console.WriteLine("Reverse file has been successfully saved");
+                    Console.WriteLine("Reverse file has been successfully saved to the " + new_path);
                 }
                 else
                 {
@@ -52,15 +59,15 @@ namespace ModuleTwo
                 switch (save)
                 {
                     case "Y":
-                        StreamWriter textfileName2 = new StreamWriter(location + @"\" + fileName2);
+                        StreamWriter textfileName2 = new StreamWriter(new_path + @"\" + fileName2);
                         textfileName2.Write(content_reverse);
                         textfileName2.Close();
-                        string[] s = File.ReadAllLines(location + @"\" + fileName2);
+                        string[] s = File.ReadAllLines(new_path + @"\" + fileName2);
                         string s1 = String.Concat<string>(s);
-                        if (File.Exists(location + @"\" + fileName2) && s1 == content_reverse)
+                        if (File.Exists(new_path + @"\" + fileName2) && s1 == content_reverse)
                         {
                             result = true;
-                            Console.WriteLine("Reverse file has been successfully saved");
+                            Console.WriteLine("Reverse file has been successfully saved to the "+new_path);
                         }
                         else
                         {
